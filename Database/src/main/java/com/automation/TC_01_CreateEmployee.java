a06029d8f9f4d655e8fb80f4575670b194b48247
Chandra <Chandra@192.168.0.7>
2019-11-20 14:22:45 +0530

    commit

--- Database/src/main/java/com/automation/TC_01_CreateEmployee.java
+++ Database/src/main/java/com/automation/TC_01_CreateEmployee.java
@@ -1,11 +1,16 @@
 package com.automation;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
 
 public class TC_01_CreateEmployee {
 	
@@ -19,19 +24,32 @@ public class TC_01_CreateEmployee {
 				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 				driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 				driver.get("https://opensource-demo.orangehrmlive.com/");
-				driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-				driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+				String username = null;
+				String password = null;
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","root");
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from users where username = 'Admin'");
+				while (resultSet.next()){
+					username = resultSet.getString(1);
+					password = resultSet.getString(2);
+					
+				}
+				connection.close();
+				
+				driver.findElement(By.id("txtUsername")).sendKeys(username);
+				driver.findElement(By.id("txtPassword")).sendKeys(password);
 				driver.findElement(By.id("btnLogin")).click();
-				driver.findElement(By.id("menu_pim_viewPimModule")).click();
+				
+		//		driver.findElement(By.id("menu_pim_viewPimModule")).click();
+			//	Thread.sleep(2000);
+			//	driver.findElement(By.id("menu_pim_addEmployee")).click();
+			//	Thread.sleep(2000);
+			//	driver.findElement(By.id("firstName")).sendKeys//
+			//	driver.findElement(By.id("lastName")).sendKeys("");
+			//	driver.findElement(By.id("btnSave")).click();
+			//	driver.findElement(By.linkText("Welcome ")).click();
+			//	driver.findElement(By.linkText("Logout")).click();
 				Thread.sleep(2000);
-				driver.findElement(By.id("menu_pim_addEmployee")).click();
-				Thread.sleep(2000);
-				driver.findElement(By.id("firstName")).sendKeys("Stephen");
-				driver.findElement(By.id("lastName")).sendKeys("Smiley");
-				driver.findElement(By.id("btnSave")).click();
-				driver.findElement(By.linkText("Welcome Admin")).click();
-				driver.findElement(By.linkText("Logout")).click();
-				Thread.sleep(5000);
 				driver.quit();
 				
 	}
